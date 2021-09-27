ac5fecbfa4a901fbfa605e0d7aa6d202c2e31694
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2021-09-27 17:27:52 -0300

    app simples em flutter

---
+++ android/app/src/main/kotlin/com/example/mdapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mdapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:mdapp/body-widget.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                currentAccountPicture: CircleAvatar(child: Text('P'),),
+                accountEmail: Text('panjos@gmail.com'),
+                accountName: Text('Panjos'),
+              ),//DrawerHeader()
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text('Home'),
+              ),
+              ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text('Perfil'),
+              ),
+              ListTile(
+                leading: Icon(Icons.notifications),
+                title: Text('Notificações'),
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Configurações'),
+              ),
+              ListTile(
+                leading: Icon(Icons.exit_to_app),
+                title: Text('Sair'),
+              ),
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          // leading: IconButton(
+          //   icon: Icon(Icons.menu),
+          //   onPressed: () {},
+          // ),
+          title: Center(
+            child: Text('Panjos'),
+          ),
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.notification_important), onPressed: () {}),
+            IconButton(icon: Icon(Icons.settings), onPressed: () {}),
+          ],
+        ),
+        body: BodyWidget(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(Icons.home),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
